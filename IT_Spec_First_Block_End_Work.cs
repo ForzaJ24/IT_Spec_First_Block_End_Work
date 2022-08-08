b5849d08c7f3b617a78a3c3afef2262992d7001c
ForzaJ24 <kuznetsovevgeniyolegovich@mail.ru>
2022-08-08 10:24:00 +0300

    int void and for

--- IT_Spec_First_Block_End_Work.cs
+++ IT_Spec_First_Block_End_Work.cs
@@ -3,6 +3,17 @@
   "Better","Faster","Stronger","Worse","Slower","Weaker"
 };
 string[] massive2 = new string[massive.Length];
-
+void SecondMassive(string[] massive, string[] massive2)
+{
+    int count = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+    if(massive[i].Length <= 3)
+        {
+        massive2[count] = massive[i];
+        count++;
+        }
+    }
+}
 
 
