0f898b7d74970f1452160cf4094817b6e1ee9468
ForzaJ24 <kuznetsovevgeniyolegovich@mail.ru>
2022-08-08 10:37:32 +0300

    works but not recognize massive elements

--- IT_Spec_First_Block_End_Work.cs
+++ IT_Spec_First_Block_End_Work.cs
@@ -1,26 +1,25 @@
-﻿string [] massive =
-{
-  "Better","Faster","Stronger","Worse","Slower","Weaker"
-};
-string[] massive2 = new string[massive.Length];
-void SecondMassive(string[] massive, string[] massive2)
+﻿// "Better","Faster","Stronger","Worse","Slower","Weaker"string [] massive =
+string[] massive1 = new string[5] {"Better", "Faster", "Stronger", "Weeker", "Slower"};
+string[] massive2 = new string[massive1.Length];
+void SecondArrayWithIF(string[] massive1, string[] massive2)
 {
     int count = 0;
-    for (int i = 0; i < massive.Length; i++)
+    for (int i = 0; i < massive1.Length; i++)
     {
-    if(massive[i].Length <= 3)
+    if(massive1[i].Length <= 3)
         {
-        massive2[count] = massive[i];
+        massive2[count] = massive1[i];
         count++;
         }
     }
 }
-void PrintArray(string[] massive)
+void PrintArray(string[] array)
 {
-    for (int i = 0; i < massive.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{massive[i]} ");
+        Console.Write($"{array[i]} ");
     }
+    Console.WriteLine();
 }
-
-
+SecondArrayWithIF(massive1, massive2);
+PrintArray(massive2);
