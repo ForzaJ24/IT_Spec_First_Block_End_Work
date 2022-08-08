629a423e99518d056e2c279e40eb6f6cd43ea8fd
ForzaJ24 <kuznetsovevgeniyolegovich@mail.ru>
2022-08-08 10:55:07 +0300

    fixing problems with elements

--- IT_Spec_First_Block_End_Work.cs
+++ IT_Spec_First_Block_End_Work.cs
@@ -1,24 +1,3 @@
-﻿string[] massive1 = new string[3] {"1", "2", "3"};
+﻿//"МТС", "Билайн", "YOTA","Мегафон","ПСБ"
+string[] massive1 = new string[5] {"МТС", "Билайн", "YOTA","Мегафон","ПСБ"};
 string[] massive2 = new string[massive1.Length];
-void SecondMassive(string[] massive1, string[] massive2)
-{
-    int count = 0;
-    for (int i = 0; i < massive1.Length; i++)
-    {
-    if(massive1[i].Length <= 3)
-        {
-        massive2[count] = massive1[i];
-        count++;
-        }
-    }
-}
-void PrintMassive(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondMassive(massive1, massive2);
-PrintMassive(massive2);
